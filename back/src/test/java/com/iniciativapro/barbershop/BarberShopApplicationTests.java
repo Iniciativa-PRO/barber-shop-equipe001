407cfb5945b4e8bd85f1976aaf3781a3628c7137
Ygor Guilherme <ygorfsguilherme@gmail.com>
2022-10-27 08:11:26 -0300

    criando a base do back end

---
+++ back/src/test/java/com/iniciativapro/barbershop/BarberShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iniciativapro.barbershop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarberShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
