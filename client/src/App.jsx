bbdcc5957c4e1c449edcedf77c9040d997add61b
Darlisson Limeira <darlisson.dev@gmail.com>
2022-10-28 13:20:29 -0300

    base client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,7 @@
-import './App.css'
+import './App.css';
 
 function App() {
-  return (
-    <h1>BarberShop App</h1>
-  )
+    return <h1>BarberShop App</h1>;
 }
 
-export default App
+export default App;
