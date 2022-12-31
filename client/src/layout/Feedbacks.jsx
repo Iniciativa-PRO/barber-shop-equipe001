bd75da58a4b2271315b1369b3649f0310dc49d28
Darlisson Limeira <darlisson.dev@gmail.com>
2022-12-31 03:31:09 -0300

    feat feedback section

--- client/src/layout/Feedbacks.jsx
+++ client/src/layout/Feedbacks.jsx
@@ -1,7 +1,16 @@
+import feedbackComment from '../assets/img/feedback-comment.svg';
+import '../assets/style/Feedbacks.css';
+
 export function Feedbacks() {
   return (
-    <>
-      <h2>Feedbacks</h2>
-    </>
+    <div className="feedbacks">
+      <div className="row feedbacks__content">
+        <img className="feedbacks__icon" src={feedbackComment} />
+        <div className="feedback">
+          <h1 className="feedback__comment">A melhor babearia da cidade!</h1>
+          <p className="feedback__costumer">Fulano de tal</p>
+        </div>
+      </div>
+    </div>
   );
 }
